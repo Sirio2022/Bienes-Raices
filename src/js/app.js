ab689ff8b109f77c815df9c4d9df609a66c97afc
Sirio2022 <105917953+Sirio2022@users.noreply.github.com>
2023-10-15 09:47:50 -0500

    DarkMode uffff!

--- src/js/app.js
+++ src/js/app.js
@@ -16,6 +16,24 @@ function navegacionResponsive() {
 }
 
 function darkMode() {
+  const prefiereDarkMode = window.matchMedia('(prefers-color-scheme: dark)');
+
+  //console.log(prefiereDarkMode.matches);
+
+  if (prefiereDarkMode.matches) {
+    document.body.classList.add('dark-mode');
+  } else {
+    document.body.classList.remove('dark-mode');
+  }
+
+  prefiereDarkMode.addEventListener('change', () => {
+    if (prefiereDarkMode.matches) {
+      document.body.classList.add('dark-mode');
+    } else {
+      document.body.classList.remove('dark-mode');
+    }
+  });
+
   const botonDarkMode = document.querySelector('.dark-mode-boton');
 
   botonDarkMode.addEventListener('click', () => {
