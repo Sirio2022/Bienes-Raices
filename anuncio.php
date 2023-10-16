e523e1821f8de281a67f6a17fd0917186f164154
Sirio2022 <105917953+Sirio2022@users.noreply.github.com>
2023-10-16 07:39:28 -0500

    Va bien!

--- anuncio.php
+++ anuncio.php
@@ -59,18 +59,9 @@
       </div>
     </main>
 
-    <footer class="footer seccion">
-      <div class="contenedor contenedor-footer">
-        <nav class="navegacion">
-          <a href="nosotros.php">Nosotros</a>
-          <a href="anuncios.php">Anuncios</a>
-          <a href="blog.php">Blog</a>
-          <a href="contacto.php">Contacto</a>
-        </nav>
-      </div>
-
-      <p class="copyright">Todos los derechos reservados 2023 &copy;</p>
-    </footer>
+    <?php
+      include 'includes/template/footer.php';
+    ?>
 
     <script src="build/js/bundle.min.js"></script>
   </body>
